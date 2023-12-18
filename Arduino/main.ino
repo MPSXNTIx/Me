92616a138d76d80d278cb939142639d471fa663b
MPSXNTIx <131395461+MPSXNTIx@users.noreply.github.com>
2023-12-17 21:44:50 -0300

    Add files via upload

---
+++ Arduino/main.ino
@@ -0,0 +1,21 @@
+// Pin
+
+// Variable
+
+void setup() {
+  // Analog
+
+  // Digital
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  mainArduino();
+  secondaryArduino();
+  delay(1000);
+}
+
+void mainArduino() {}
+
+void secondaryArduino() {}

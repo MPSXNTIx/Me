07215a65e78c8cf6d38595230cb3ce46ba52fde3
MPSXNTIx <131395461+MPSXNTIx@users.noreply.github.com>
2023-12-24 23:39:54 -0300

    Update main Pantalla LCD + I2C.ino

--- Arduino/main Pantalla LCD + I2C.ino
+++ Arduino/main Pantalla LCD + I2C.ino
@@ -1,1 +1,19 @@
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(9600);
+}
+
+void loop() {
+  mainArduino();
+  delay(1000);
+}
+
+void mainArduino() {
+  lcd.setCursor(0,0);
+  lcd.print("Hola Mundo");
+}
